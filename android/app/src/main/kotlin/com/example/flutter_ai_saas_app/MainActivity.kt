af3b676374d7c7bce56acca58ac3b5d57dfed82b
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-09-15 13:11:35 +0400

    stripe setup requirements

--- android/app/src/main/kotlin/com/example/flutter_ai_saas_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ai_saas_app/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.flutter_ai_saas_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
